c6901c629013e2f5b34e568bc5d71a6876de3832
Justin Guese <guese.justin@gmail.com>
2022-03-11 12:04:43 +0100

    adding secrets 2 env

--- template/python3-fastapi/function/handler.py
+++ template/python3-fastapi/function/handler.py
@@ -1,13 +1,24 @@
 from fastapi import HTTPException
 from pydantic import BaseModel
 from typing import Dict
+import glob
+from os import environ
 
 FUNCTION_NAME = 'templatefn'
 FUNCTION_VERSION = '1.0.0'
 FUNCTION_SUMMARY = "A function that does this"
 FUNCTION_RESPONSE_DESC = "Definition of object returned by function"
 
-
+# reads in secrets to environment variables, such that they can be 
+# easily used with environ["SECRET_NAME"]
+def readSecretToEnv(secretpath):
+    secretname = secretpath.split('/')[-1]
+    with open(secretpath, "r") as f:
+        environ[secretname] = f.read()
+        
+for secret in glob.glob("/var/openfaas/secrets/*"):
+    readSecretToEnv(secret)
+    
 class RequestModel(BaseModel):
     data: Dict
 
